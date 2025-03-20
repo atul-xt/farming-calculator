2faa67aa830456d7adf5f33fc11000d8e2b80067
atul-xt <atul.xtt@gmail.com>
2025-03-20 21:52:23 +0530

    build successfully

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='py-2 border-b border-gray-300 shadow-sm'>
+        <img className='w-24 m-auto' src="./images/LOGO.webp" alt="LOGO" />
+    </div>
+  )
+}
+
+export default Navbar
